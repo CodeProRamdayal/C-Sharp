e81e80dad1936abbd53659a01b4af02cba69e62d
RamDayal Patel <ramdayal.patel45120@gmail.com>
2026-08-18 22:33:48 +0530

    Day 11 understanding the getter and setter in oops

---
+++ Csharp_Tutorials/OopsConcepts/ClassAndObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_Tutorials.OopsConcepts
+{
+    internal class Car
+    {
+        private string _model = "";
+        private string _brand = "";
+        private string _name = "";
+        private int age = 0;
+        private bool buyCar;
+
+        public Car(string name, string model, string brand)
+        {
+            Name = name;
+            Model = model;
+            Brand = brand;
+            Console.WriteLine("Name : {0} Model : {1}  Brand : {2}",_name,_model,_brand);
+        }
+
+  
+        public string Name { get => _name; set => _name = value; }
+        public string Brand { get => _brand; set => _brand = value; }
+        public string Model { get => _model; set => _model = value; }
+        public int Age { get => age; set => age = value; }
+        public bool BuyCar { get{
+                if (Age > 18)
+                {
+                    return true;
+                }
+                else { return false; }
+            }
+            set => buyCar = value; }
+
+
+        public static void MyCarDetail(out string name,out int amount)
+        {
+            name = "My Brand Car";
+            amount =1000;
+        }
+
+        public static void MyBikeDetail(ref string bikeName, ref int amount)
+        {
+
+            bikeName = "Classic 350";
+            amount = 30000;
+
+        }
+    }
+}
--- Csharp_Tutorials/Program.cs
+++ Csharp_Tutorials/Program.cs
@@ -1,6 +1,7 @@
 ﻿// QuizApp
 using Csharp_Tutorials.Arrays;
 using Csharp_Tutorials.Methods;
+using Csharp_Tutorials.OopsConcepts;
 using Csharp_Tutorials.QuizApp;
 //string question1 = "What is the capital of France?";
 //string answer1 = "Paris";
@@ -152,8 +153,42 @@ var inAndOutRef = new Csharp_Tutorials.Variables.InAndOutREF();
 //Console.WriteLine("Factorial of 5: {0}", factorialOf5);
 
 // Weather Station Simulator
-WeatherStationSimulator weatherStation = new WeatherStationSimulator();
+//WeatherStationSimulator weatherStation = new WeatherStationSimulator();
 
-weatherStation.WaatherCondition();
+//weatherStation.WaatherCondition();
+
+// Day 9
+// Oops Concepts
+
+Car audi = new Car("Audi Serices" ,"AI19","AUDI");
+Car bmw = new Car("BMW serices", "i7", "BMW");
+audi.Model = "I912";
+
+Console.WriteLine("Audi" , audi.Model) ;
+Console.WriteLine("Audi" + audi.Model);
+    audi.Age = 10;
+ audi.BuyCar = true;
+if (audi.Age < 18)
+{
+    Console.WriteLine("My son you are not eliagiable for drive the car : " + (audi.BuyCar.Equals(false)? "NO":""));
+}
+else
+{
+    Console.WriteLine("Yes, You are eligiable to drive car." + (audi.BuyCar.Equals(true) ? "Yes":""));
+}
 
 
+var carName = "Audi with gold color";
+var carAmount = 2000;
+
+var bikeName = "Davison 200";
+var bikeAmount = 1400;
+Console.WriteLine("Car Deatils before the Method calling"+ carName  + " "+carAmount);
+
+ Car.MyCarDetail(out carName, out carAmount);
+Console.WriteLine("Car Deatils after the Method calling"+carName + " " +carAmount);
+
+Console.WriteLine("Bike Deatils before the Method calling" + bikeName + " " + bikeAmount);
+
+Car.MyBikeDetail(ref bikeName, ref bikeAmount);
+Console.WriteLine("Bike Deatils after the Method calling" + bikeName + " " + bikeAmount);
